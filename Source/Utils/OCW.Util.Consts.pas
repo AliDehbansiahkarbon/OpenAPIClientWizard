811c0e49a0f4c5801689f3c4328dcf147618209b
Ali Dehbansiahkarbon <adehban@gmail.com>
2024-09-03 23:56:37 +0300

    Added a missing file.

---
+++ Source/Utils/OCW.Util.Consts.pas
@@ -0,0 +1,55 @@
+  unit OCW.Util.Consts;
+
+interface
+
+const
+  Utf8AcceptCharSet = 'utf-8';
+  RestDefaultTimeOut = 30000; //ms
+
+  cJson_RequestBody = 'requestbody';
+  cJson_Description = 'description';
+  cJson_Required = 'required';
+  cJson_Content = 'content';
+  cJson_Schema = 'schema';
+  cJson_Example = 'example';
+  cJson_Properties = 'properties';
+  cJson_ApplicationJson = 'application/json';
+  cJson_ApplicationFormUrlencoded = 'application/x-www-form-urlencoded';
+  cJson_Parameters = 'parameters';
+  cJson_type = 'type';
+  cJson_name = 'name';
+  cJson_In = 'in';
+  cJson_Summary = 'summary';
+  cJson_Operationid = 'operationid';
+
+
+  cPostman_Key = 'key';
+  cPostman_Value = 'value';
+  cPostman_Description = 'description';
+  cPostman_Query = 'query';
+  cPostman_Info = 'info';
+  cPostman_Variables = 'variables';
+  cPostman_Item = 'item';
+  cPostman_Request = 'request';
+  cPostman_Name = 'name';
+  cPostman_Method = 'method';
+  cPostman_Auth = 'auth';
+  cPostman_Type = 'type';
+  cPostman_Bearer = 'bearer';
+  cPostman_Url = 'url';
+  cPostman_Raw = 'raw';
+  cPostman_Protocol = 'protocol';
+  cPostman_Host = 'host';
+  cPostman_Path = 'path';
+  cPostman_variable = 'variable';
+  cPostman_Header = 'header';
+  cPostman_Body = 'body';
+  cPostman_mode = 'mode';
+  cPostman_Options = 'options';
+  cPostman_Response = 'response';
+  cPostman_ProtocolProfileBehavior = 'protocolProfileBehavior';
+  cPostman_DisableBodyPruning = 'disableBodyPruning';
+
+implementation
+
+end.
